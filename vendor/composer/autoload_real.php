fd9e73dfb9c1aafeef867f8385fec4f641533fdd
David Grudl <david@grudl.com>
2021-01-14 23:52:37 +0100

    preloaded Nette 3.1 (2020-01-14)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit282f65bd5f240d12c712912b97f1835d
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit282f65bd5f240d12c712912b97f1835d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit282f65bd5f240d12c712912b97f1835d', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit282f65bd5f240d12c712912b97f1835d::getInitializer($loader));
         } else {
